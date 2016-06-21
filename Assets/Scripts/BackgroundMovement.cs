d6ab703eb0ac1722437e689f479d91a49911baae
Ronald Albert <ronaldalbert1609@gmail.com>
2016-06-20 21:58:33 -0300

    FirstCommitOfGradiusGame
    FirstCommit background moving and player is also moving

---
+++ Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    public float speed = 0.1f;
+	void Start ()
+    {
+	
+	}
+	void Update ()
+    {
+        Vector2 offset = new Vector2(speed * Time.time, 0);
+        this.GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float mvx;
+    float mvy;
+    public float speed;
+	void Start ()
+    {
+        speed = 5;
+	}
+	void Update ()
+    {
+        mvx = Input.GetAxis("Horizontal");
+        mvy = Input.GetAxis("Vertical");
+        this.transform.position += new Vector3(mvx * speed * Time.deltaTime, mvy * speed * Time.deltaTime);
+	}
+}
